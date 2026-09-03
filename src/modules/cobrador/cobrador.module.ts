8cb936ac0e7e1ca53c5ac7f936fc599ea5e06654
CobraIA Dev <dev@cobraiadev.local>
2026-09-02 23:19:56 -0500

    fix(cobrador): importar SociosModule para resolver PermisosSocioService

--- src/modules/cobrador/cobrador.module.ts
+++ src/modules/cobrador/cobrador.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthModule } from "../auth/auth.module";
 import { CarteraModule } from "../cartera/cartera.module";
 import { CobradoresModule } from "../cobradores/cobradores.module";
+import { SociosModule } from "../socios/socios.module";
 import { Ruta } from "../rutas/ruta.entity";
 import { RutasModule } from "../rutas/rutas.module";
 import { CobradorController } from "./cobrador.controller";
@@ -14,6 +15,7 @@ import { CobradorService } from "./cobrador.service";
     CarteraModule,
     RutasModule,
     CobradoresModule,
+    SociosModule,
     AuthModule,
   ],
   controllers: [CobradorController],
